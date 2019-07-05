650519b393b20e70ab8cd36797a01634bdeb9e64
Mariana <nilomariana@gmail.com>
2019-07-05 18:40:30 -0300

    functions

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-
 const store = {
-    sellers =["Ada", "Grace", "Hedy", "Sheryl"],
-    sales =[
+    sellers: ["Ada", "Grace", "Hedy", "Sheryl"],
+    sales: [
         // tener en cuenta que Date guarda los meses del 0 (enero) al 11 (diciembre)
         { saleDate: new Date(2019, 1, 4), seller: "Grace", pieces: ["Monitor GPRS 3000", "Motherboard ASUS 1500"], store: 'Centro' },
         { saleDate: new Date(2019, 0, 1), seller: "Ada", pieces: ["Monitor GPRS 3000", "Motherboard ASUS 1500"], store: 'Centro' },
         { saleDate: new Date(2019, 0, 2), seller: "Grace", pieces: ["Monitor ASC 543", "Motherboard MZI"], store: 'Centro' },
         { saleDate: new Date(2019, 0, 10), seller: "Ada", pieces: ["Monitor ASC 543", "Motherboard ASUS 1200"], store: 'Centro' },
         { saleDate: new Date(2019, 0, 12), seller: "Grace", pieces: ["Monitor GPRS 3000", "Motherboard ASUS 1200"], store: 'Centro' },
-        { saleDate: new Date(2019, 1, 12), seller: "Hedy", pieces: ["Monitor GPRS 3000", "HDD Toshiba"], store: 'Centro' },
+        { saleDate: new Date(2019, 1, 12), seller: "Hedy", pieces: ["Monitor GPRS 3000", "HDD Toshiba"], store: 'Centro' }, 
         { saleDate: new Date(2019, 1, 24), seller: "Shery", pieces: ["Motherboard ASUS 1200", "HDD Western Digital"], store: 'Caballito' },
         { saleDate: new Date(2019, 1, 1), seller: "Ada", pieces: ["Motherboard MZI", "RAM Kingston Fury"], store: 'Centro' },
         { saleDate: new Date(2019, 1, 11), seller: "Grace", pieces: ["Monitor ASC 543", "RAM Kingston"], store: 'Caballito' },
@@ -35,28 +34,59 @@ const store = {
         { piece: "RAM Kingston", price: 110 },
         { piece: "RAM Kingston Fury", price: 230 }
     ],
-    stores: ['Centro', 'Caballito']
-}
+    stores: ['Centro','Caballito']
+    };
 
-/*recorre las piezas vendidas según el mes*/
-const monthSales = (month)=> {
-    let monthData = []
-    store.pieces.forEach( sale => {
-        let sales = store.sales.filter( sale => sale.pieces === pieces);
-        let totalPieces = monthPieces ( month, year, pieces);
-        monthData.push({piecesName: pieces, total:''})
+// creo un array con las propiedades del objeto donde guardo la información de la tienda
+var storeProperties = Object.keys(store)
+// hago la variable de contenido de los selects global para que esté disponible para todas las instancias
+let selectContent, newSale;
+const monthsNames = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','aiciembre']
+const weekDays = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado']
+const piecesList = []
+const {prices} = store
+
+//1.precioMaquina(componentes): 
+let pcPrice = sale => sale.length>0?sale.map(e => store.prices.find(({piece}) => e === piece).price).reduce((a,b)=>a+b):0
+//probando
+const maquina = ["Monitor GPRS 3000", "Monitor GPRS 3000"]
+console.log(`(1) La venta de ${maquina} tiene un valor total de ARS ${pcPrice(maquina)}`)
+
+//2.ventasMes(mes, año)
+let monthSales = (year, month) => 
+  pcPrice(store.sales.filter(({saleDate})=>saleDate.getFullYear()===year && saleDate.getMonth()===month-1).map(({pieces})=>pieces).flat())
+//probando
+const mes= 1
+const anio=2019
+console.log(`(2) Las ventas del mes ${mes} de ${anio} ascienden a ARS ${monthSales(anio,mes)}`)
+
+//3.vendedoraDelMes(mes, año)
+let sellerMonth = (year, month) => {
+    const monthSales = store.sales.filter(({saleDate})=>saleDate.getFullYear()===year && saleDate.getMonth()===month-1)
+    const sellerMonth =store.sellers.map(name =>{
+      return { name:name, sales:pcPrice(monthSales.filter(({seller})=>seller===name).map(({pieces})=>pieces).flat())}
+    })
+   const bestSale = Math.max(...sellerMonth.map(({sales})=>sales).flat())
+   const sellerSalesList = sellerMonth.filter(({sales})=> sales>= bestSale).map(({name})=>name).flat()
+    return sellerSalesList
     }
-}
-const initialize = ()=> {
-    let mainDiv = document.getElementById('monthSeller')
-}
+//probando
+const anio2=2019
+const mes2=2
+console.log (`(3) La mejor vendedora del mes ${mes2} de ${anio2} es ${sellerMonth(anio2,mes2)}`)
 
-/* recorre las ventas de cada vendedora en toda la lista */
-const sellerSales = (month, year) =>{
-    let sellersData = []
-    store.sellers.forEach( seller => {
-        let sales = store.sales.filter( sale => sale.seller === seller)
-        let totalSales = monthSales(month, year, sales)
-        sellersData.push({sellerName:seller, total:''})
-    });
-}
+//4.vendedoraDelMes(mes, año)
+let storeMonth = (year, month) => {
+    const monthSales = store.sales.filter(({saleDate})=>saleDate.getFullYear()===year && saleDate.getMonth()===month-1)
+    const storeMonth =store.stores.map(name =>{
+      return { name:name, sales:pcPrice(monthSales.filter(({store})=>store===name).map(({pieces})=>pieces).flat())}
+    })
+   const bestSale = Math.max(...storeMonth.map(({sales})=>sales).flat())
+   const storeSalesList = storeMonth.filter(({sales})=> sales>= bestSale).map(({name})=>name).flat()
+    return storeSalesList
+    }
+//probando
+const anio3=2019
+const mes3=1
+console.log (`(4) La mejor sucursal del mes ${mes3} de ${anio3} es ${storeMonth(anio3,mes3)}`)
+
