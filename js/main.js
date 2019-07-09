bfdb4f9895ea45ed03cd9ccda98944bce7333ad9
Mariana <nilomariana@gmail.com>
2019-07-09 13:19:36 -0300

    reordenando js

--- js/main.js
+++ js/main.js
@@ -41,17 +41,185 @@ const store = {
 var storeProperties = Object.keys(store)
 // hago la variable de contenido de los selects global para que esté disponible para todas las instancias
 let selectContent, newSale;
-const monthsNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'aiciembre']
-const weekDays = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
+const monthsNames = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','aiciembre']
+const weekDays = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado']
 const piecesList = []
-const { prices } = store
+const {prices} = store
+
+// crea los selects de la sección que define vendedora y sucursal y componentes
+const setSelects = () => {
+    storeProperties.forEach (e => {
+        if (e === 'sellers' || e === "stores") {
+            let container = document.getElementById('primarySelects')
+            let select = document.createElement('select')
+            select.id = e
+            select.setAttribute('onchange',`setSaleElement(this,'saleTopLine')`)
+            container.appendChild(select)
+            selectContent = store[e];
+            fillSelects(selectContent,select)
+        } else if (e === "prices"){
+            let container = document.getElementById('pieceSelect')
+            let select = document.createElement('select')
+            select.id = 'pieces'
+            container.appendChild(select)
+            let selectContent = store[e].map(item => item.piece)
+            fillSelects(selectContent,select)
+        }
+    })
+}
+
+// establezco en pantalla los elementos de la venta
+const setSaleElement = (select,containerId) => {
+    let selectedElement = select.options[select.value].text
+    let container = document.getElementById(containerId)
+    let saleElement = document.createElement('p')
+    saleElement.setAttribute('id',`act${select.id}`)
+    saleElement.innerText = selectedElement
+    container.appendChild(saleElement)
+}
+
+const setSaleDate = () => {
+    let actualDate = new Date()
+    let container = document.getElementById('saleDate')
+    container.innerHTML=''
+    let date = document.createElement('p')
+    date.innerText = `${weekDays[actualDate.getDay()]}, ${actualDate.getDate()} de ${monthsNames[actualDate.getMonth()]} de ${actualDate.getFullYear()}`
+    container.appendChild(date)
+}
+
+// rellena los selects
+const fillSelects = (list,select) => {
+    list.forEach(e => {
+        if (select.childElementCount === 0) {
+            let placeholder = { name: `seleccione ${select.id}`, id: '' }
+            select.appendChild(createOption(placeholder))
+        }
+        let optionItem = {name: e, id:list.indexOf(e) }
+        select.appendChild(createOption(optionItem))
+    })
+}
+
+// crea las opciones de cada select
+const createOption = elem => {
+    let option = document.createElement('option')
+    option.innerText = elem.name
+    option.value = elem.id+1
+    return option
+}
+
+// agregar el boton con el que se agregan las piezas a la venta
+const addBtn = (containerElem,btnText,btnId,btnAttribute,btnFunction) => {
+    let container = document.getElementById(containerElem)
+    let btn = document.createElement('button')
+    btn.innerText = btnText
+    btn.id = btnId
+    btn.setAttribute(btnAttribute,btnFunction)
+    container.appendChild(btn)
+}
+
+const setElements = () => {
+    setSelects();
+    addBtn('pieceSelect','Agregar Pieza','addPiece','onclick','addPiece()');
+    setSaleDate();
+}
+
+const pcPrice = (piecesList) => {
+    // debugger;
+    let saleTotalPrice = 0
+    piecesList.forEach(e => {
+        saleTotalPrice += e.price
+    })
+    return saleTotalPrice   
+}
+
+const showPieceList = (rowContent) => {
+    let container =document.getElementById('piecesDetails')
+    container.innerHTML=''
+    rowContent.forEach(e =>{
+        let c1LineId = e.lineId
+        let c2PieceDesc = e.piece
+        let c3Price = e.price
+        // let rowHtml = '<tr><td>${lineId}</td><td>${pieceDesc}</td><td>${linePrice}</td>'
+        let row = document.createElement('tr')
+        let lineIdCol = document.createElement('td')
+        let pieceCol = document.createElement('td')
+        let priceCol = document.createElement('td')
+        lineIdCol.innerText=c1LineId
+        pieceCol.innerText=c2PieceDesc
+        priceCol.innerText=c3Price
+        row.appendChild(lineIdCol)
+        row.appendChild(pieceCol)
+        row.appendChild(priceCol)       
+        container.appendChild(row)
+    })
+    
+}
+
+const showSaleItems = () => {
+
+    showPieceList(piecesList)
+    let totalContainer = document.getElementById('saleTotal')
+    totalContainer.innerHTML=''
+    let totalPriceItem = document.createElement('p')
+    totalPriceItem.innerText = `Precio Total de la Venta: ${pcPrice(piecesList)}`
+    totalContainer.appendChild(totalPriceItem)
+    addBtn('saleTotal','Registrar Venta','btnAddSale','onclick','addSale()')
+}
+
+const findPrice = (list,elem) =>{
+    let pieceToAdd = list.filter(thisPiece => (thisPiece.piece == elem))
+    let price = pieceToAdd[0].price
+    return price
+}
+
+const addPiece = () => {
+    let sale = {lineId:'',piece:'',price:0 }
+    let select = document.getElementById('pieces')
+    let selectedPiece = select.options[select.value].text
+    let id = piecesList.length
+    sale.lineId = `00${++id}`
+    sale.piece = selectedPiece
+    sale.price = findPrice(prices,selectedPiece)    
+    piecesList.push(sale)
+    // console.log(piecesList)
+    showSaleItems()   
+}
+
+const clearOptions = () => {
+    let sellerSelect = document.getElementById('sellers')
+    sellerSelect.value=0
+}
+
+const clearActualSale = () => {
+    // esto funciona para reiniciar la página pero si no hay local storage pierde la venta añadida
+    location.reload() 
+}
+
+const addSale = () => {    
+    const actualSale = { saleDate: new Date(), seller:'', pieces: [], store: '' }
+    let todayDate = new Date()
+    let todayMonth = todayDate.getMonth()
+    let todayYear = todayDate.getFullYear()
+    let todayDay = todayDate.getDate()
+    actualSale.seller = document.getElementById('actsellers').innerText
+    actualSale.store = document.getElementById('actstores').innerText
+    actualSale.saleDate = new Date (todayYear,todayMonth,todayDay)
+    actualSale.pieces = piecesList.map(e => e.piece)
+    store.sales.push(actualSale)
+    debugger;
+    console.log (store.sales)
+    clearOptions()
+    clearActualSale()
+}
+
+// Chequea Local Storage
+const checkStorage = () => {
+    let storedSales = window.localStorage.getItem('actualSale')
+    console.log(storedSales)
+}
+
 //------------------REPORT.HTML---------------------
 //------------------MONTH-REPORT
-//1.precioMaquina(componentes): 
-const pcPrice = sale => sale.length > 0 ? sale.map(e => store.prices.find(({ piece }) => e === piece).price).reduce((a, b) => a + b) : 0
-//probando
-const maquina = ["Monitor GPRS 3000", "Monitor GPRS 3000"]
-console.log(`(1) La venta de ${maquina} tiene un valor total de ARS ${pcPrice(maquina)}`)
 
 //2.ventasMes(mes, año)
 const monthSales = (year, month) => pcPrice(store.sales.filter(({ saleDate }) => saleDate.getFullYear() === year && saleDate.getMonth() === month - 1).map(({ pieces }) => pieces).flat())
@@ -91,15 +259,6 @@ const mes3 = 1
 console.log(`(4) La mejor sucursal del mes ${mes3} de ${anio3} es ${storeMonth(anio3, mes3)}`)
 
 //SELECTORES Y BOTONES DE REPORT.HTML
-
-//I.-Crear Opcion
-const createOption = comp => {
-  typeof comp === "object" ? e = comp.item : e = comp
-  const option = document.createElement("option")
-  option.innerText = e
-  option.value = e
-  return option
-}
 
 //II.- Rellenar Select
 const fillSelects = (list,select) => {
@@ -184,7 +343,7 @@ const btnStoreMonth = e => {
     printResult(`(ノಠ益ಠ)ノ彡┻━┻ NO HUBO VENTAS`, "result")
   }
 }
-//inicialización del programa
+//inicialización del programa report.html
 const initialize = () => {
   setSecondSelects()
   setSecondSelects(store.pieces, "pieces")
@@ -192,32 +351,6 @@ const initialize = () => {
   setSecondSelects(store.stores, "bstore")
 }
 //------------------MONTH-REPORT-END-------------------
-
-const addSale = () => {    
-    const actualSale = { saleDate: new Date(), seller:'', pieces: [], store: '' }
-    let todayDate = new Date()
-    let todayMonth = todayDate.getMonth()
-    let todayYear = todayDate.getFullYear()
-    let todayDay = todayDate.getDate()
-    actualSale.seller = document.getElementById('actsellers').innerText
-    actualSale.store = document.getElementById('actstores').innerText
-    actualSale.saleDate = new Date (todayYear,todayMonth,todayDay)
-    actualSale.pieces = piecesList.map(e => e.piece)
-    store.sales.push(actualSale)
-    console.log (store.sales)
-
-    let aux = JSON.stringify(actualSale)
-    window.localStorage.setItem('actualSale', aux)
-
-    clearOptions()
-    clearActualSale()
-}
-
-// Chequea Local Storage
-const checkStorage = () => {
-    let storedSales = window.localStorage.getItem('actualSale')
-    console.log(storedSales)
-}
 
 //2. cantidadVentasComponente(componente) = qtySoldByPiece. Calcula la cantidad de Ventas por Componente.
 
